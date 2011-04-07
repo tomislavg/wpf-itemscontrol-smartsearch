2d821004e2f90590fb4c6028ae18f0481069341a
bruno.chappe <bruno.chappe@08632c28-77f5-bf08-4ee1-505a9887af84>
2011-04-07 12:56:51 +0000

    Changed one the collection to a simple List<T> instead of ObservableCollection<T> to demonstrate that filter works on both collection types

--- trunk/dotnetexplorer.blog.com.WPFIcRtSandFc.SampleApp/SampleViewModel.cs
+++ trunk/dotnetexplorer.blog.com.WPFIcRtSandFc.SampleApp/SampleViewModel.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.ObjectModel;
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
 
 namespace dotnetexplorer.blog.com.WPFIcRtSandFc.SampleApp
 {
@@ -10,12 +11,13 @@ namespace dotnetexplorer.blog.com.WPFIcRtSandFc.SampleApp
         public SampleViewModel()
         {
             itemsPooler = new ItemsPooler();
-            DataSourceStrategies = itemsPooler.GetStrategiesSourceScopeOne();
+            DataSourceStrategies = new List<StrategyAdapter>(itemsPooler.GetStrategiesSourceScopeOne());
             DataSourceMarkets = itemsPooler.GetMarketsSourceScopeOne();
         }
 
-
-        public ObservableCollection<StrategyAdapter> DataSourceStrategies { get; set; }
+        //2 types of collections to demonstrate that static filter works on both types of collections
+        //However, the first one won't notify smart search when receiving or removing items
+        public List<StrategyAdapter> DataSourceStrategies { get; set; }
         public ObservableCollection<MarketAdapter> DataSourceMarkets { get; set; }
 
     }
