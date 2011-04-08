2ebd72a12c2b71e871fca2013597b42927ba3930
bruno.chappe <bruno.chappe@08632c28-77f5-bf08-4ee1-505a9887af84>
2011-04-08 07:52:50 +0000

    Added a omitted converter

---
+++ dotnetexplorer.blog.com.WPFIcRtSandFc.SampleApp/SampleStringConverter.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Windows.Data;
+
+namespace dotnetexplorer.blog.com.WPFIcRtSandFc.SampleApp
+{
+    public class SampleStringConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if(value!=null)
+            {
+                var str = value.ToString();
+
+
+                str = str.Insert(2, ".");
+                str = str.Insert(4, ".");
+                str = str.Insert(8, ".");
+                return str;
+            }
+            return string.Empty;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
