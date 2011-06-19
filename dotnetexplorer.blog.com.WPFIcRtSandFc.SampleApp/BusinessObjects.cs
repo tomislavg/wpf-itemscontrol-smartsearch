ebd11c081e74aacecdc199ea9a0185768c9e758e
bruno.chappe <bruno.chappe@08632c28-77f5-bf08-4ee1-505a9887af84>
2011-06-19 12:29:07 +0000

    - Modified the sample solution to provide an example of real time filtering on objects add/remove/propertyChanged

--- dotnetexplorer.blog.com.WPFIcRtSandFc.SampleApp/BusinessObjects.cs
+++ dotnetexplorer.blog.com.WPFIcRtSandFc.SampleApp/BusinessObjects.cs
@@ -3,68 +3,191 @@
 // --------------------------------------------------------------------------------------------------------------------
 
 using System;
+using System.ComponentModel;
 
 namespace dotnetexplorer.blog.com.WPFIcRtSandFc.SampleApp
 {
     /// <summary>
     /// The strategy adapter.
     /// </summary>
-    public class StrategyAdapter
+    public class StrategyAdapter : INotifyPropertyChanged
     {
+        private DateTime _sendTime;
+
         /// <summary>
         ///   Gets or sets SendTime.
         /// </summary>
-        public DateTime SendTime { get; set; }
+        public DateTime SendTime
+        {
+            get { return _sendTime; }
+            set
+            {
+                _sendTime = value;
+                InvokePropertyChanged(new PropertyChangedEventArgs("SendTime"));
+            }
+        }
+
+        private StrategyStatus _stratStat;
 
         /// <summary>
         ///   Gets or sets StratStat.
         /// </summary>
-        public StrategyStatus StratStat { get; set; }
+        public StrategyStatus StratStat
+        {
+            get { return _stratStat; }
+            set
+            {
+                _stratStat = value;
+
+                InvokePropertyChanged(new PropertyChangedEventArgs("StratStat"));
+            }
+        }
+
+        private StrategyType _stratType;
 
         /// <summary>
         ///   Gets or sets StratType.
         /// </summary>
-        public StrategyType StratType { get; set; }
+        public StrategyType StratType
+        {
+            get { return _stratType; }
+            set
+            {
+                _stratType = value;
+                InvokePropertyChanged(new PropertyChangedEventArgs("StratType"));
+            }
+        }
+
+        private Direction _dir;
 
         /// <summary>
         ///   Gets or sets Dir.
         /// </summary>
-        public Direction Dir { get; set; }
+        public Direction Dir
+        {
+            get { return _dir; }
+            set
+            {
+                _dir = value;
+                InvokePropertyChanged(new PropertyChangedEventArgs("Dir"));
+            }
+        }
+
+        private string _message;
 
         /// <summary>
         ///   Gets or sets Message.
         /// </summary>
-        public string Message { get; set; }
+        public string Message
+        {
+            get { return _message; }
+            set
+            {
+                _message = value;
+                InvokePropertyChanged(new PropertyChangedEventArgs("Message"));
+            }
+        }
+
+        private string _product;
 
         /// <summary>
         ///   Gets or sets Product.
         /// </summary>
-        public string Product { get; set; }
+        public string Product
+        {
+            get { return _product; }
+            set
+            {
+                _product = value;
+                InvokePropertyChanged(new PropertyChangedEventArgs("Product"));
+            }
+        }
+
+        private long _executedAmount;
 
         /// <summary>
-        ///   Gets or sets Amount.
+        ///   Gets or sets ExecutedAmount.
         /// </summary>
-        public long Amount { get; set; }
+        public long ExecutedAmount
+        {
+            get { return _executedAmount; }
+            set
+            {
+                _executedAmount = value;
+                InvokePropertyChanged(new PropertyChangedEventArgs("ExecutedAmount"));
+            }
+        }
+
+        private long _requestedAmount;
 
         /// <summary>
         ///   Gets or sets RequestedAmount.
         /// </summary>
-        public long RequestedAmount { get; set; }
+        public long RequestedAmount
+        {
+            get { return _requestedAmount; }
+            set
+            {
+                _requestedAmount = value;
+                InvokePropertyChanged(new PropertyChangedEventArgs("RequestedAmount"));
+            }
+        }
+
+        private decimal _executedPrice;
 
         /// <summary>
-        ///   Gets or sets Price.
+        ///   Gets or sets ExecutedPrice.
         /// </summary>
-        public decimal Price { get; set; }
+        public decimal ExecutedPrice
+        {
+            get { return _executedPrice; }
+            set
+            {
+                _executedPrice = value;
+                InvokePropertyChanged(new PropertyChangedEventArgs("ExecutedPrice"));
+            }
+        }
+
+        private decimal _requestedPrice;
 
         /// <summary>
         ///   Gets or sets RequestedPrice.
         /// </summary>
-        public decimal RequestedPrice { get; set; }
+        public decimal RequestedPrice
+        {
+            get { return _requestedPrice; }
+            set
+            {
+                _requestedPrice = value;
+                InvokePropertyChanged(new PropertyChangedEventArgs("RequestedPrice"));
+            }
+        }
+
+        private string _markets;
 
         /// <summary>
         ///   Gets or sets Markets.
         /// </summary>
-        public string Markets { get; set; }
+        public string Markets
+        {
+            get { return _markets; }
+            set
+            {
+                _markets = value;
+                InvokePropertyChanged(new PropertyChangedEventArgs("Markets"));
+            }
+        }
+
+        private bool _isManual;
+        public bool IsManual
+        {
+            get { return _isManual; }
+            set
+            {
+                _isManual = value;
+                InvokePropertyChanged(new PropertyChangedEventArgs("IsManual"));
+            }
+        }
 
         /// <summary>
         /// The to string.
@@ -74,8 +197,36 @@ namespace dotnetexplorer.blog.com.WPFIcRtSandFc.SampleApp
         /// </returns>
         public override string ToString()
         {
-            return string.Format("SendTime: {0}, StratStat: {1}, StratType: {2}, Dir: {3}, Product: {4}", SendTime, 
-                                 StratStat, StratType, Dir, Product);
+            return string.Format("SendTime: {0}, StratStat: {1}, StratType: {2}, Dir: {3}, Product: {4}, IsManuel : {5}", SendTime,
+                                 StratStat, StratType, Dir, Product, IsManual);
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void InvokePropertyChanged(PropertyChangedEventArgs e)
+        {
+            PropertyChangedEventHandler handler = PropertyChanged;
+            if (handler != null) handler(this, e);
+        }
+
+        public StrategyAdapter Clone()
+        {
+            return new StrategyAdapter
+                       {
+                           Dir = Dir,
+                           IsManual = IsManual,
+                           SendTime = SendTime,
+                           StratStat = StratStat,
+                           ExecutedAmount = ExecutedAmount,
+                           RequestedAmount = RequestedAmount,
+                           RequestedPrice = RequestedPrice,
+                           ExecutedPrice = ExecutedPrice,
+                           Message = Message,
+                           Product = Product,
+                           Markets = Markets,
+
+                           StratType = StratType
+                       };
         }
     }
 
@@ -88,22 +239,22 @@ namespace dotnetexplorer.blog.com.WPFIcRtSandFc.SampleApp
         /// <summary>
         ///   The in progress.
         /// </summary>
-        InProgress, 
+        InProgress = 0,
 
         /// <summary>
         ///   The done.
         /// </summary>
-        Done, 
+        Done = 1,
 
         /// <summary>
         ///   The in error.
         /// </summary>
-        InError, 
+        InError = 2,
 
         /// <summary>
         ///   The cancelled.
         /// </summary>
-        Cancelled, 
+        Cancelled = 3,
     }
 
     /// <summary>
@@ -114,17 +265,17 @@ namespace dotnetexplorer.blog.com.WPFIcRtSandFc.SampleApp
         /// <summary>
         ///   The ioc sweep.
         /// </summary>
-        IOCSweep, 
+        IOCSweep = 0,
 
         /// <summary>
         ///   The gtc sweep.
         /// </summary>
-        GTCSweep, 
+        GTCSweep = 1,
 
         /// <summary>
         ///   The simple order.
         /// </summary>
-        SimpleOrder, 
+        SimpleOrder = 2,
     }
 
     /// <summary>
@@ -135,12 +286,12 @@ namespace dotnetexplorer.blog.com.WPFIcRtSandFc.SampleApp
         /// <summary>
         ///   The buy.
         /// </summary>
-        Buy, 
+        Buy = 0,
 
         /// <summary>
         ///   The sell.
         /// </summary>
-        Sell
+        Sell = 1,
     }
 
     /// <summary>
@@ -151,12 +302,12 @@ namespace dotnetexplorer.blog.com.WPFIcRtSandFc.SampleApp
         /// <summary>
         ///   The institutional.
         /// </summary>
-        Institutional, 
+        Institutional,
 
         /// <summary>
         ///   The corporate.
         /// </summary>
-        Corporate, 
+        Corporate,
     }
 
     /// <summary>
@@ -179,4 +330,12 @@ namespace dotnetexplorer.blog.com.WPFIcRtSandFc.SampleApp
         /// </summary>
         public MarketType Type { get; set; }
     }
+
+
+    public enum ExecutionMarkets
+    {
+        EBS, REUTERS, AUTHOBAN, BLOOMBERG, HSBC
+    }
+
+
 }
--- dotnetexplorer.blog.com.WPFIcRtSandFc.SampleApp/SampleViewModel.cs
+++ dotnetexplorer.blog.com.WPFIcRtSandFc.SampleApp/SampleViewModel.cs
@@ -2,31 +2,142 @@
 // http://dotnetexplorer.blog.com
 // --------------------------------------------------------------------------------------------------------------------
 
+using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Windows.Threading;
 
 namespace dotnetexplorer.blog.com.WPFIcRtSandFc.SampleApp
 {
     /// <summary>
     /// The sample view model.
     /// </summary>
-    internal class SampleViewModel
+    internal class SampleViewModel : INotifyPropertyChanged
     {
+        private readonly RelayCommand startPooling;
+        private readonly RelayCommand _stopPooling;
         /// <summary>
         ///   The items pooler.
         /// </summary>
         private readonly ItemsPooler itemsPooler;
 
+        private Dispatcher _uiDispatcher;
+        private RelayCommand _clearData;
+        private bool isPooling;
+
         /// <summary>
         ///   Initializes a new instance of the <see cref = "SampleViewModel" /> class.
         /// </summary>
-        public SampleViewModel()
+        public SampleViewModel(Dispatcher dispatcher)
         {
-            itemsPooler = new ItemsPooler();
+            _uiDispatcher = dispatcher;
+            itemsPooler = new ItemsPooler(_uiDispatcher);
+            itemsPooler.AddItem += ItemsPoolerAddItem;
+            itemsPooler.RemoveItem += ItemsPoolerRemoveItem;
+            itemsPooler.AddLog += ItemsPoolerAddLog;
             DataSourceStrategies = new List<StrategyAdapter>(itemsPooler.GetStrategiesSourceScopeOne());
             DataSourceMarkets = itemsPooler.GetMarketsSourceScopeOne();
             DataSourceString = itemsPooler.GetStringSourceScopeOne();
             DataSourceInt = itemsPooler.GetIntSourceScopeOne();
+            startPooling = new RelayCommand(ExecuteStartPooling);
+            _stopPooling = new RelayCommand(ExecuteStopPooling);
+            _clearData = new RelayCommand(ExecuteClearData);
+            _insertItem = new RelayCommand(ExecuteInsertItem);
+            RealTimeDataSourceStrategies = new ObservableCollection<StrategyAdapter>();
+            InvokePropertyChanged(new PropertyChangedEventArgs("LstMarkets"));
+            InvokePropertyChanged(new PropertyChangedEventArgs("ProductsReferential"));
+            InvokePropertyChanged(new PropertyChangedEventArgs("StrategyStatusReferential"));
+            InvokePropertyChanged(new PropertyChangedEventArgs("StrategyTypesReferential"));
+            InvokePropertyChanged(new PropertyChangedEventArgs("DirectionReferential"));
+            InvokePropertyChanged(new PropertyChangedEventArgs("ManualItem"));
+
+
+        }
+
+        private void ExecuteInsertItem(object obj)
+        {
+            itemsPooler.InsertManualItem(ManualItem);
+        }
+
+        void ItemsPoolerAddLog(string message)
+        {
+            _uiDispatcher.Invoke(new Action<string>(DoLog), message);
+        }
+
+        private void DoLog(string obj)
+        {
+            Logs = Logs += string.Format("{0}{1}", Environment.NewLine, obj);
+        }
+
+        void ItemsPoolerRemoveItem(StrategyAdapter obj)
+        {
+            _uiDispatcher.Invoke(new Action<StrategyAdapter>(RemoveItem), obj);
+        }
+
+        private void RemoveItem(StrategyAdapter obj)
+        {
+            RealTimeDataSourceStrategies.Remove(obj);
+        }
+
+        private void ExecuteClearData(object obj)
+        {
+            if (!isPooling)
+            {
+                itemsPooler.Clear();
+                var count = RealTimeDataSourceStrategies.Count;
+                for (int i = count - 1; i > -1; i--)
+                {
+                    RealTimeDataSourceStrategies.RemoveAt(i);
+                }
+                Logs = string.Empty;
+            }
+
+           
+           
+        }
+
+        void ItemsPoolerAddItem(StrategyAdapter obj)
+        {
+
+            _uiDispatcher.Invoke(new Action<StrategyAdapter>(AddItem), obj);
+        }
+
+        void AddItem(StrategyAdapter strategyAdapter)
+        {
+            RealTimeDataSourceStrategies.Add(strategyAdapter);
+        }
+
+        public RelayCommand StartPooling
+        {
+            get { return startPooling; }
+        }
+
+        public RelayCommand StopPooling
+        {
+            get { return _stopPooling; }
+        }
+
+        public RelayCommand ClearData
+        {
+            get { return _clearData; }
+        }
+
+        public RelayCommand InsertItem
+        {
+            get { return _insertItem; }
+        }
+
+        private void ExecuteStopPooling(object obj)
+        {
+            itemsPooler.StopPooling();
+            isPooling = false;
+        }
+
+        private void ExecuteStartPooling(object obj)
+        {
+            isPooling = true;
+            itemsPooler.StartPooling();
         }
 
         // 2 types of collections to demonstrate that static filter works on both types of collections
@@ -51,5 +162,65 @@ namespace dotnetexplorer.blog.com.WPFIcRtSandFc.SampleApp
         ///   Gets or sets DataSourceInt.
         /// </summary>
         public IEnumerable<int> DataSourceInt { get; set; }
+
+        public ObservableCollection<StrategyAdapter> RealTimeDataSourceStrategies { get; set; }
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void InvokePropertyChanged(PropertyChangedEventArgs e)
+        {
+            PropertyChangedEventHandler handler = PropertyChanged;
+            if (handler != null) handler(this, e);
+        }
+
+
+        private string _logs;
+        public string Logs
+        {
+            get { return _logs; }
+            set
+            {
+                _logs = value;
+                InvokePropertyChanged(new PropertyChangedEventArgs("Logs"));
+            }
+        }
+
+        private StrategyAdapter _manualItem;
+        private RelayCommand _insertItem;
+
+        public StrategyAdapter ManualItem
+        {
+            get
+            {
+                return _manualItem ?? (_manualItem = new StrategyAdapter());
+            }
+            set { _manualItem = value; InvokePropertyChanged(new PropertyChangedEventArgs("ManualItem")); }
+        }
+
+        public List<string> LstMarkets
+        {
+            get { return itemsPooler.LstMarkets; }
+        }
+
+        public List<string> ProductsReferential
+        {
+            get { return itemsPooler.ProductsReferential; }
+        }
+
+        public List<StrategyStatus> StrategyStatusReferential
+        {
+            get { return itemsPooler.StrategyStatusReferential; }
+        }
+
+        public List<StrategyType> StrategyTypesReferential
+        {
+            get { return itemsPooler.StrategyTypesReferential; }
+        }
+
+        public List<Direction> DirectionReferential
+        {
+            get { return itemsPooler.DirectionReferential; }
+        }
     }
 }
